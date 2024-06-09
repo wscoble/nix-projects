abb931fb0ddb8f809630a8e6e1a85be13d5b89fd
Scott Scoble <scott@scoble.me>
2024-06-08 19:28:53 -0700

    update services

--- home/nixos/configuration.nix
+++ home/nixos/configuration.nix
@@ -135,11 +135,11 @@
 
       displayManager = {
         lightdm.enable = true;
-        setupCommands = ''
-          ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
-        '';
+        # setupCommands = ''
+        #   ${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal
+        # '';
         autoLogin = {
-          inherit user;
+          user = "sscoble";
           enable = true;
         };
       };
